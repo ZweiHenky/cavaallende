f120b25bfcc5eda0272fe5470d9ac9f5070ce0a9
ZweiHenky <ronnhy7@gmail.com>
2026-01-23 02:48:58 -0600

    add icons

---
+++ assets/icons/MinusIcon.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Svg, { Path } from "react-native-svg";
+
+export default function MinusIcon({ size = 24, color = "#000", ...props }) {
+  return (
+    <Svg
+      width={size}
+      height={size}
+      viewBox="0 0 640 640"
+      fill="none"
+      {...props}
+    >
+      <Path
+        fill={color}
+        d="M96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320z"
+      />
+    </Svg>
+  );
+};
+
+
